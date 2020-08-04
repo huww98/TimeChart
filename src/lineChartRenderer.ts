24f9a998347710ce49e85aa5f64bca92c20c5e81
胡玮文 <huww98@outlook.com>
2020-08-04 17:25:00 +0800

    Tested on iOS safari

--- src/lineChartRenderer.ts
+++ src/lineChartRenderer.ts
@@ -25,15 +25,15 @@ void main() {
     gl_Position = vec4(pos2d, 0, 1);
 }`;
 
-    if (gl instanceof WebGL2RenderingContext) {
-        return `#version 300 es
-layout (location = ${VertexAttribLocations.DATA_POINT}) in vec2 aDataPoint;
-layout (location = ${VertexAttribLocations.DIR}) in vec2 aDir;
-${body}`;
-    } else {
+    if (gl instanceof WebGLRenderingContext) {
         return `
 attribute vec2 aDataPoint;
 attribute vec2 aDir;
+${body}`;
+    } else {
+        return `#version 300 es
+layout (location = ${VertexAttribLocations.DATA_POINT}) in vec2 aDataPoint;
+layout (location = ${VertexAttribLocations.DIR}) in vec2 aDir;
 ${body}`;
     }
 }
@@ -41,7 +41,14 @@ ${body}`;
 function fsSource(gl: WebGL2RenderingContext | WebGLRenderingContext) {
     const body = `
 `;
-    if (gl instanceof WebGL2RenderingContext) {
+    if (gl instanceof WebGLRenderingContext) {
+        return `
+precision lowp float;
+uniform vec4 uColor;
+void main() {
+    gl_FragColor = uColor;
+}`;
+    } else {
         return `#version 300 es
 precision lowp float;
 uniform vec4 uColor;
@@ -49,13 +56,7 @@ out vec4 outColor;
 void main() {
     outColor = uColor;
 }`;
-    } else {
-        return `
-precision lowp float;
-uniform vec4 uColor;
-void main() {
-    gl_FragColor = uColor;
-}`;
+
     }
 }
 
@@ -162,15 +163,15 @@ class SeriesSegmentVertexArray {
             gl.enableVertexAttribArray(VertexAttribLocations.DIR);
             gl.vertexAttribPointer(VertexAttribLocations.DIR, 2, gl.FLOAT, false, BYTES_PER_POINT, 2 * Float32Array.BYTES_PER_ELEMENT);
         }
-        if (gl instanceof WebGL2RenderingContext) {
-            this.vao = new WebGL2VAO(gl);
-        } else {
-            // const vaoExt = gl.getExtension('OES_vertex_array_object');
-            // if (vaoExt) {
-            //     this.vao = new OESVAO(vaoExt);
-            // } else {
+        if (gl instanceof WebGLRenderingContext) {
+            const vaoExt = gl.getExtension('OES_vertex_array_object');
+            if (vaoExt) {
+                this.vao = new OESVAO(vaoExt);
+            } else {
                 this.vao = new WebGL1BufferInfo(bindFunc);
-            // }
+            }
+        } else {
+            this.vao = new WebGL2VAO(gl);
         }
         bindFunc();
 

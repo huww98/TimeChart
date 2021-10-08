315607714d2c2e9c131834c3f4007282cb0d51a6
Valerio De Benedetto <debevv@users.noreply.github.com>
2021-10-08 21:12:51 +0200

    Flipping tooltip at the edges

--- src/tooltip.ts
+++ src/tooltip.ts
@@ -80,8 +80,17 @@ td {
         el.addEventListener('mousemove', ((ev: MouseEvent) => {
             const contentRect = el.getBoundingClientRect();
             const tooltipRect = this.tooltip.getBoundingClientRect();
-            ls.right = `${contentRect.width - (ev.clientX - contentRect.x) + mouseOffset}px`
-            ls.top = `${(ev.clientY - contentRect.y - tooltipRect.height - mouseOffset)}px`
+            let right = contentRect.width - (ev.clientX - contentRect.x) + mouseOffset;
+            let top = (ev.clientY - contentRect.y - tooltipRect.height - mouseOffset);
+
+            if (right + tooltipRect.width + mouseOffset > (contentRect.right))
+                right = right - tooltipRect.width - mouseOffset*2;
+
+            if (top < 0)
+                top = top + tooltipRect.height + mouseOffset*2;
+
+            ls.right = right + "px";
+            ls.top = top + "px";
         }).bind(this));
 
         el.addEventListener('mouseenter', ev => {
